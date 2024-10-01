f0199cc3b05098838da50e9843da2ec00df9db63
Andy Pak <im.andy.pak@gmail.com>
2024-10-01 19:22:14 -0400

    Fix small issue on projects page

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -15,7 +15,7 @@ function Projects() {
                     <Image src={planetPal} alt='PlanetPal image cover' className='w-xl md:w-2xl h-auto border-solid rounded-2xl border-8 border-black'></Image>
                 </div>
                 <div>
-                    <Link href='/projects/planetpal' className='border-solid border-4 border-black text-white bg-blue-300 rounded-xl p-2 hover:bg-blue-500 hover:shadow-xl transition duration-600 ease-in-out'>Click to Learn More</Link>
+                    <Link href='https://devpost.com/software/planetpal-thq27l' target='_blank' className='border-solid border-4 border-black text-white bg-blue-300 rounded-xl p-2 hover:bg-blue-500 hover:shadow-xl transition duration-600 ease-in-out'>Click to Learn More</Link>
                 </div>
             </div>
             
@@ -25,20 +25,20 @@ function Projects() {
                     <Image src={stroom} alt='Stroom image cover' className='w-xl md:w-2xl h-auto border-solid rounded-2xl border-8 border-black'></Image>
                 </div>
                 <div>
-                    <Link href='/projects/stroom' className='border-solid border-4 border-black text-white bg-blue-300 rounded-xl p-2 hover:bg-blue-500 hover:shadow-xl transition duration-600 ease-in-out'>Click to Learn More</Link>
+                    <Link href='https://devpost.com/software/stroom-rjk8im' target='_blank' className='border-solid border-4 border-black text-white bg-blue-300 rounded-xl p-2 hover:bg-blue-500 hover:shadow-xl transition duration-600 ease-in-out'>Click to Learn More</Link>
                 </div>
             </div>
 
             <div className='flex flex-col justify-center items-center gap-7 border-solid border-8 border-blue-950 rounded-2xl p-5 text-center bg-slate-100'>
                 <div>
-                    <div className='text-3xl'>PlanetPal</div>
+                    <div className='text-3xl'>Menti</div>
                     <dir>WINNER at Serenity Hacks 2024</dir>
                 </div>
                 <div>
                     <Image src={menti} alt='Menti image cover' className='w-xl md:w-2xl h-auto border-solid rounded-2xl border-8 border-black'></Image>
                 </div>
                 <div>
-                    <Link href='/projects/menti' className='border-solid border-4 border-black text-white bg-blue-300 rounded-xl p-2 hover:bg-blue-500 hover:shadow-xl transition duration-600 ease-in-out'>Click to Learn More</Link>
+                    <Link href='https://devpost.com/software/menti-g3k5jp' target='_blank' className='border-solid border-4 border-black text-white bg-blue-300 rounded-xl p-2 hover:bg-blue-500 hover:shadow-xl transition duration-600 ease-in-out'>Click to Learn More</Link>
                 </div>
             </div>
 
