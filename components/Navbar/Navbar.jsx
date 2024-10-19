df129ea13d91cc54d95eb14eb5476290c473d0c2
Andy Pak <im.andy.pak@gmail.com>
2024-10-19 18:10:38 -0400

    Change conditions for mobile navbar and add custom media query

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,26 +1,11 @@
 'use client'
 import Link from 'next/link'
 import Navlogo from './Navlogo'
-import { useState, useRef, useEffect } from 'react'
+import styles from './Navbar.module.css'
+import { useState} from 'react'
 
 function Navbar() {
     const [navbar, setNavbar] = useState(false);
-    const expandRef = useRef(null);
-    const buttonRef = useRef(null);
-
-    useEffect(() => {
-        const clickOutside = (e) => {
-            if (expandRef.current && !expandRef.current.contains(e.target)) {
-                setNavbar(false);
-            }
-        };
-
-        document.addEventListener('mousedown', clickOutside);
-
-        return () => {
-            document.removeEventListener('mousedown', clickOutside);
-        }
-    }, [navbar, expandRef]);
 
     const routes = [
         {
@@ -45,22 +30,28 @@ function Navbar() {
         <div className='bg-black flex justify-between items-center px-5 py-4 h-24'>
             <Navlogo />
 
-            <div className='md:hidden'>
-                <button 
-                    onClick={() => setNavbar(!navbar)}
-                    className='text-white p-2 focus:outline-none hover:bg-gray-500 rounded-lg transition duration-600 ease-in-out'
-                >
-                    {navbar ? '✖' : '☰'}
-                </button>
+            <button 
+                onClick={() => setNavbar(prevNavbar => !prevNavbar)}
+                className={`${styles.mobileButton} hidden text-white p-2 focus:outline-none hover:bg-gray-500 rounded-lg transition duration-600 ease-in-out`}
+            >
+                {navbar ? '✖' : '☰'}
+            </button>
+            <div className={`${navbar ? 'bg-slate-600 absolute top-24 inset-x-0 flex flex-col items-center justify-center w-screen' : 'hidden'}`}>
+                {routes.map((route) => (
+                    <Link className='w-screen text-center p-4 hover:bg-gray-500 rounded-lg transition duration-600 ease-in-out text-white' 
+                        href={route.path} key={route.title}
+                    >
+                        {route.title}
+                    </Link>
+                ))}
             </div>
 
             <div 
                 id='links'
-                className={`flex-col md:flex md:flex-row md:gap-5 absolute md:relative left-0 w-full md:w-auto bg-black px-5 py-2 text-1xl text-center ${navbar ? 'block top-20' : 'hidden'}`}
-                ref={expandRef}
+                className={`${styles.regularNavbar} flex-col md:flex md:flex-row md:gap-5 absolute md:relative left-0 w-full md:w-auto bg-black px-5 py-2 text-1xl text-center`}
             >
                 {routes.map((route) => (
-                    <Link className='hover:bg-gray-500 p-2 rounded-lg transition duration-600 ease-in-out text-white' 
+                    <Link className='p-2 hover:bg-gray-500 rounded-lg transition duration-600 ease-in-out text-white' 
                         href={route.path} key={route.title}
                     >
                         {route.title}
