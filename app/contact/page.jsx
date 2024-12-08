d2c4c263e5470db8ef0e884e005ea8e49a2e4a55
Andy Pak <im.andy.pak@gmail.com>
2024-12-07 23:46:57 -0500

    Update contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,10 +1,23 @@
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
 function Contact() {
     return (
-        <div className="text-center text-5xl flex items-center justify-center flex-col flex-grow gap-8">
-            <div>Contact page is WORK IN PROGRESS</div>
-            <div>Look in footer theres literally links right there</div>
+        <div className="flex flex-col justify-center items-center flex-grow text-center">
+            <div className="text-center my-5 mt-8 text-3xl">
+                <Link href="https://www.linkedin.com/in/andy-pak/" className="text-[#0e76a8] no-underline ml-1 hover:text-[#377491] transition-all ease-in-out">
+                    <FontAwesomeIcon icon={faLinkedin} className="mr-1" />
+                    {`LinkedIn`}
+                </Link>
+                {` is the best way to get in touch with me.`}
+            </div>
+            <div className="text-center text-5xl flex items-center justify-center flex-col flex-grow gap-8">
+                <div>Contact page is WORK IN PROGRESS</div>
+                <div>Check out other ways to reach me in the footer</div>
+            </div>
         </div>
     );
 }
 
-export default Contact
+export default Contact;
