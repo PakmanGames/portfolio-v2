ba75aee3071d5a78a3dc3731312a63f6c82a234e
Andy Pak <im.andy.pak@gmail.com>
2024-12-30 11:43:41 -0500

    Embed resume

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,7 +1,7 @@
 function Resume() {
     return (
-        <div>
-            hi
+        <div className="flex flex-1 items-center m-5 justify-center z-10">
+            <iframe src="/Andy-Pak-Resume.pdf" frameborder="0" className="border-4 rounded border-black w-[50vw] h-[80vh]"></iframe>
         </div>
     );
 }
