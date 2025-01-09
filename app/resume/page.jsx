c9d67b6a0b5c00962f7791059d7ab29cc6ed4c49
Andy Pak <im.andy.pak@gmail.com>
2025-01-08 21:44:13 -0500

    Create new resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,8 +1,8 @@
+import PDFViewer from "@/components/pdf-viewer";
+
 function Resume() {
     return (
-        <div className="flex flex-1 items-center m-5 justify-center z-10">
-            <iframe src="/Andy-Pak-Resume.pdf" frameborder="0" className="border-4 rounded border-black w-[50vw] h-[80vh]"></iframe>
-        </div>
+        <PDFViewer pdfUrl={`/Andy-Pak-Resume.pdf`} title={`Andy Pak's Resume`} />
     );
 }
 
