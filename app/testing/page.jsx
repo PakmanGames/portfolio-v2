8bc901190ce612de92954def1139a8ffce228deb
Andy Pak <im.andy.pak@gmail.com>
2025-01-08 23:24:59 -0500

    Add project data

--- app/testing/page.jsx
+++ app/testing/page.jsx
@@ -2,8 +2,12 @@ import ProjectCard from "@/components/ProjectCard";
 
 export default function TestingPage() {
     return (
-        <div>
-            <ProjectCard id={1} title="Project 1" image="/images/planetpal.jpg" description="This is a description of project 1" learnMore="https://devpost.com/software/planetpal-thq27l" />
+        <div className="z-10">
+            <ProjectCard 
+                id={1} title="Project 1" 
+                image="/images/planetpal.jpg" 
+                description="This is a description of project 1" 
+                learnMore="https://devpost.com/software/planetpal-thq27l" />
         </div>
     );
 }
---
+++ data/projects.js
@@ -0,0 +1,30 @@
+export const projects = [
+    {
+        id: 1,
+        title: "PlanetPal",
+        image: "/images/planetpal.jpg",
+        description: "Eco-friendly companion app that helps users reduce their carbon footprint. Created at TerraHacks 2024.",
+        learnMore: "https://devpost.com/software/planetpal-thq27l"
+    },
+    {
+        id: 2,
+        title: "Stroom",
+        image: "/images/stroom.jpg",
+        description: "Platform to host study sessions and collaborate with peers.",
+        learnMore: "https://devpost.com/software/stroom-rjk8im"
+    },
+    {
+        id: 3,
+        title: "Menti",
+        image: "/images/menti.jpg",
+        description: "🏆 Winner at SerenityHacks 2024. A habit tracking app with AI suggestions and feedback.",
+        learnMore: "https://devpost.com/software/menti-g3k5jp"
+    },
+    {
+        id: 4,
+        title: "More Projects",
+        image: "/images/moreprojects.png",
+        description: "More projects to be added soon!",
+        learnMore: "/projects"
+    }
+]
