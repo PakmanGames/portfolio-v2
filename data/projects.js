c9e4b0426ae27e31f8ef13b1d062cd0e3b2e8cb7
Andy Pak <im.andy.pak@gmail.com>
2025-01-09 23:49:01 -0500

    Update data with projects

--- data/projects.js
+++ data/projects.js
@@ -1,30 +1,108 @@
+// Images are around 500 x 200 px
 export const projects = [
     {
         id: 1,
-        title: "PlanetPal",
-        image: "/images/planetpal.jpg",
-        description: "Eco-friendly companion app that helps users reduce their carbon footprint.",
-        learnMore: "https://devpost.com/software/planetpal-thq27l"
+        title: "Moonlighter",
+        image: "/images/moonlighter.png",
+        description: "A 2D top-down dungeon crawler game written in Java, inspired by the game Moonlighter.",
+        learnMore: "https://github.com/PakmanGames/Moonlighter"
     },
     {
         id: 2,
-        title: "Stroom",
-        image: "/images/stroom.jpg",
-        description: "Platform to host study sessions and collaborate with peers.",
-        learnMore: "https://devpost.com/software/stroom-rjk8im"
-    },
-    {
-        id: 3,
         title: "Menti",
         image: "/images/menti.jpg",
         description: "🏆 Winner at SerenityHacks 2024. A habit tracking app with AI suggestions and feedback.",
         learnMore: "https://devpost.com/software/menti-g3k5jp"
     },
     {
+        id: 3,
+        title: "Stroom",
+        image: "/images/stroom.jpg",
+        description: "Platform to host study sessions and collaborate with peers.",
+        learnMore: "https://devpost.com/software/stroom-rjk8im"
+    },
+    {
         id: 4,
-        title: "More Projects",
-        image: "/images/moreprojects.png",
-        description: "More projects to be added soon!",
-        learnMore: "/projects"
-    }
+        title: "Genshin Recipes",
+        image: "/images/recipes.png",
+        description: "My first website ever created, a simple website for some Genshin Impact Recipes.",
+        learnMore: "https://pakmangames.github.io/odin-recipes/"
+    },
+    {
+        id: 5,
+        title: "Rock Paper Scissors",
+        image: "/images/rockpaperscissors.png",
+        description: "Challenge the RPS-3000 to an intense game of Rock Paper Scissors! My first project using JavaScript.",
+        learnMore: "https://pakmangames.github.io/odin-rock-paper-scissors/"
+    },
+    {
+        id: 6,
+        title: "Calculator",
+        image: "/images/calculator.png",
+        description: "🤓 A standard calculator. Incorporates keyboard support, custom sounds, and animations.",
+        learnMore: "https://pakmangames.github.io/calculator/",
+    },
+    {
+        id: 7,
+        title: "Banana",
+        image: "/images/banana.png",
+        description: "🍌 A webpage all about bananas, yes seriously.",
+        learnMore: "https://pakmangames.github.io/landing-page/",
+    },
+    {
+        id: 8,
+        title: "Etch-A-Sketch",
+        image: "/images/etchasketch.png",
+        description: "🎨 A digital Etch-A-Sketch. Draw, erase, and set grid size. Choose custom or random colours.",
+        learnMore: "https://pakmangames.github.io/etch-a-sketch/",
+    },
+    {
+        id: 9,
+        title: "Image Slider",
+        image: "/images/kurumiimageslider.png",
+        description: "A simple image slider. Learned more about state in React and various hooks.",
+        learnMore: "https://github.com/PakmanGames/react-image-slider",
+    },
+    {
+        id: 10,
+        title: "PlanetPal",
+        image: "/images/planetpal.jpg",
+        description: "Eco-friendly companion app that helps users reduce their carbon footprint.",
+        learnMore: "https://devpost.com/software/planetpal-thq27l"
+    },
+    {
+        id: 11,
+        title: "Ruby Tic-Tac-Toe",
+        image: "/images/rubytictactoe.png",
+        description: "A simple console based Tic-Tac-Toe game written in Ruby.",
+        learnMore: "https://github.com/PakmanGames/ruby-tic-tac-toe",
+    },
+    {
+        id: 12,
+        title: "Ruby Mastermind",
+        image: "/images/rubymastermind.png",
+        description: "A simple console based Mastermind game written in Ruby.",
+        learnMore: "https://github.com/PakmanGames/ruby-mastermind",
+    },
+    {
+        id: 13,
+        title: "Gatekeeper",
+        image: "/images/gatekeeper.png",
+        description: "A simple password manager written in C, integrates OpenSSL and SQLite.",
+        learnMore:"https://github.com/PakmanGames/gatekeeper",
+    },
+    {
+        id: 14,
+        title: "AnonAlyze",
+        image: "/images/anonalyze.png",
+        description: "🏆 Winner of Best Security Hack at CTRL+HACK+DEL. An app that scans Instagram data to reveal info you might be unknowingly sharing.",
+        learnMore: "https://devpost.com/software/anonalyze"
+    },
+    {
+        id: 15,
+        title: "Not The New York Times",
+        image: "/images/notthenewyorktimes.png",
+        description: "🏆 Winner at HackWestern 11. Delivering fake news like never before in a satirical and witty way",
+        learnMore: "https://dorahacks.io/buidl/20398"
+    },
 ]
