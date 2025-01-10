3d0fc2ba11f81ab007e078c56fa220638fca9c2f
Andy Pak <im.andy.pak@gmail.com>
2025-01-09 22:59:56 -0500

    Fix issue with projects reversing

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -2,11 +2,13 @@ import ProjectCard from '@/components/ProjectCard';
 import { projects } from '@/data/projects';
 
 function Projects() {
+  const displayProjects = [...projects].reverse();
+
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-8 text-center">{`PakmanGames' Projects`}</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {projects.reverse().map((project) => (
+        {displayProjects.map((project) => (
           <div 
             className="z-10"
             key={project.id}>
