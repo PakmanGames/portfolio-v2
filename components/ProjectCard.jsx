95b840820e18018c8009c786219e02f1d107c978
Andy Pak <im.andy.pak@gmail.com>
2025-01-22 16:20:03 -0500

    Change how images are shown

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 function ProjectCard({ title, image, description, learnMore }) {
     return (
         <div className="bg-[#f4f4ff] rounded-lg shadow-lg overflow-hidden">
-            <Image src={image} alt={title} width={400} height={200} className="w-full h-48 object-cover" />
+            <Image src={image} alt={title} width={400} height={200} className="w-full h-48 object-contain bg-[#dedefd]" />
             <div className="p-4">
                 <h3 className="text-xl font-semibold mb-2">{title}</h3>
                 <p className="text-gray-600 mb-4">{description}</p>
